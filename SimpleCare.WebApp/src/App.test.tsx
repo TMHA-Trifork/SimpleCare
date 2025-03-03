ecc5900b4a474fa5d1ac70133e270f5f0ceb317d
rlftrifork <rlf@trifork.com>
2025-03-03 08:41:58 +0100

    Add vite project insted of cra project as frontend

--- SimpleCare.WebApp/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders Simpel care text', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Simpel care/i)
-  expect(linkElement).toBeInTheDocument()
-})
