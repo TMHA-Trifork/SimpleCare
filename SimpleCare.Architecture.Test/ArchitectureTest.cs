6264a26884e3d54392410fdbd2a95f3ca08361a0
Torben Mejlvang Hagensen <132343194+TMHA-Trifork@users.noreply.github.com>
2025-03-19 17:02:56 +0100

    Added architecture test for boundary projects and improved domain project test

--- SimpleCare.Architecture.Test/ArchitectureTest.cs
+++ SimpleCare.Architecture.Test/ArchitectureTest.cs
@@ -19,6 +19,11 @@ public class ArchitectureTest
         foreach (var domainAssembly in domainAssemblies)
         {
             var allReferenced = domainAssembly.GetReferencedAssemblies();
+
+            var infrastructureReference = allReferenced.FirstOrDefault(assemblyName => assemblyName.Name?.EndsWith(".Infrastructure.Interfaces") ?? false);
+
+            Assert.True(infrastructureReference is null, $"{domainAssembly.GetName().Name} has a reference to infrastructure interface library");
+
             var referenced = allReferenced
                 .Where(assemblyName =>
                     ((assemblyName.Name?.EndsWith(".Domain") ?? false) && (assemblyName.Name != domainAssembly.GetName().Name))||
@@ -56,6 +61,37 @@ public class ArchitectureTest
                 .Where(assemblyName => (assemblyName.Name?.EndsWith(".Domain") ?? false) && (assemblyName.Name != domainName));
 
             Assert.False(otherDomainReferences.Any(), $"{applicationAssembly.GetName().Name} has references to other modules domain library");
+        }
+    }
+
+    [Fact]
+    public void BoundaryLibrariesShouldNotReferenceAnyOtherSolutionLibraries()
+    {
+        var apiAssembly = typeof(EmergencyWardController).Assembly;
+
+        var assemblyNames = apiAssembly.GetReferencedAssemblies();
+
+        var domainAssemblies = assemblyNames
+            .Where(assemblyName => assemblyName.Name?.EndsWith(".Boundary") ?? false)
+            .Select(assemblyName => Assembly.Load(assemblyName));
+
+        foreach (var boundaryAssembly in domainAssemblies)
+        {
+            var allReferenced = boundaryAssembly.GetReferencedAssemblies();
+
+            var infrastructureReference = allReferenced.FirstOrDefault(assemblyName =>
+                (assemblyName.Name?.EndsWith(".Infrastructure") ?? false) ||
+                (assemblyName.Name?.EndsWith(".Infrastructure.Interfaces") ?? false));
+
+            Assert.True(infrastructureReference is null, $"{boundaryAssembly.GetName().Name} has a reference to infrastructure (interface) library");
+
+            var referenced = allReferenced
+                .Where(assemblyName =>
+                    (assemblyName.Name?.EndsWith(".Domain") ?? false) ||
+                    (assemblyName.Name?.EndsWith(".Application") ?? false) ||
+                    ((assemblyName.Name?.EndsWith(".Boundary") ?? false) && (assemblyName.Name != boundaryAssembly.GetName().Name)));
+
+            Assert.False(referenced.Any(), $"{boundaryAssembly.GetName().Name} has references to other module class libraries");
         }
     }
 }
