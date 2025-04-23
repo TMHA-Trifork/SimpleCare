d7825b39d1c1265ac373eb8e26f79a471226b62c
Torben Mejlvang Hagensen <132343194+TMHA-Trifork@users.noreply.github.com>
2025-04-23 15:03:13 +0200

    Added tests

--- SimpleCare.EmergencyWards.Tests/EmergencyWardRootTest.cs
+++ SimpleCare.EmergencyWards.Tests/EmergencyWardRootTest.cs
@@ -11,7 +11,7 @@ public class EmergencyWardRootTest(EmergencyWardRootTestFixture fixture) : IClas
     {
         var patient = await fixture.EmergencyWard.GetPatient(new Guid(EmergencyWardRootTestFixture.PatientId1), CancellationToken.None);
 
-        Assert.Equal(new Guid(PatientTestFixture.PatientId1), patient.Id);
+        Assert.Equal(new Guid(EmergencyWardRootTestFixture.PatientId1), patient.Id);
     }
 
     [Fact]
@@ -38,9 +38,9 @@ public class EmergencyWardRootTest(EmergencyWardRootTestFixture fixture) : IClas
     }
 
     [Theory]
-    [InlineData(EmergencyPatientStatus.Registered, PatientTestFixture.PatientId1)]
-    [InlineData(EmergencyPatientStatus.InTransfer, PatientTestFixture.PatientId2)]
-    [InlineData(EmergencyPatientStatus.Discharged, PatientTestFixture.PatientId3)]
+    [InlineData(EmergencyPatientStatus.Registered, EmergencyWardRootTestFixture.PatientId1)]
+    [InlineData(EmergencyPatientStatus.InTransfer, EmergencyWardRootTestFixture.PatientId2)]
+    [InlineData(EmergencyPatientStatus.Discharged, EmergencyWardRootTestFixture.PatientId3)]
     public async Task GetAll_ShouldReturnPatientWithCorrectStatus_WhenStatusIsGiven(EmergencyPatientStatus status, Guid patientId)
     {
         var patients = await fixture.EmergencyWard.GetPatients([status], CancellationToken.None);
@@ -49,7 +49,7 @@ public class EmergencyWardRootTest(EmergencyWardRootTestFixture fixture) : IClas
     }
 
     [Fact]
-    public async Task Register_ShouldCreateNewPatientAndEncounter()
+    public async Task RegisterPatient_ShouldAddNewPatientAndEncounter()
     {
         var personalIdentifier = "123456789";
         var familyName = "Doe";
@@ -61,13 +61,79 @@ public class EmergencyWardRootTest(EmergencyWardRootTestFixture fixture) : IClas
         Assert.NotNull(encounter);
         Assert.Equal(observation, encounter.EncounterReason);
 
-        //await fixture.PatientRepository.Received().Add(
-        //    Arg.Is<Patient>(p => p.)
-            //,
+        await fixture.PatientRepository.Received().Add(
+            Arg.Is<Patient>(p =>
+                p.PersonalIdentifier == personalIdentifier &&
+                p.FamilyName == familyName &&
+                p.GivenNames == givenNames &&
+                p.Status == EmergencyPatientStatus.Registered),
+            Arg.Any<CancellationToken>());
 
         await fixture.EncounterRepository.Received().Add(
             Arg.Is<Encounter>(e => e.EncounterReason == observation),
             Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task TransferPatient_ShouldSetPatientInTransferAndReturnEvent_IfPatientIsRegistered()
+    {
+        var patientId = new Guid(EmergencyWardRootTestFixture.PatientId1);
+        var personalIdentifier = "123456789";
+        var familyName = "Doe";
+        var givenNames = "John";
+        var wardIdentifier = "M1";
+        var reason = "Transfer to another ward";
+
+        var transferredEvent = await fixture.EmergencyWard.TransferPatient(patientId, wardIdentifier, reason, CancellationToken.None);
+
+        Assert.Equal(personalIdentifier, transferredEvent.PersonalIdentifier);
+        Assert.Equal(familyName, transferredEvent.FamilyName);
+        Assert.Equal(givenNames, transferredEvent.GivenNames);
+        Assert.Equal(wardIdentifier, transferredEvent.WardIdentifier);
+        Assert.Equal(reason, transferredEvent.Reason);
+
+        await fixture.PatientRepository.Received().Update(
+            Arg.Is<Patient>(p =>
+                p.Id == patientId &&
+                p.PersonalIdentifier == personalIdentifier &&
+                p.FamilyName == familyName &&
+                p.GivenNames == givenNames &&
+                p.Status == EmergencyPatientStatus.InTransfer &&
+                p.WardIdentifier == wardIdentifier),
+            Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task TransferPatient_ShouldFail_IfPatientIsInTransfer()
+    {
+        var patientId = new Guid(EmergencyWardRootTestFixture.PatientId2);
+        var wardIdentifier = "M1";
+        var reason = "Transfer to another ward";
+
+        await Assert.ThrowsAnyAsync<Exception>(async () =>
+            await fixture.EmergencyWard.TransferPatient(patientId, wardIdentifier, reason, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task TransferPatient_ShouldFail_IfPatientIsDischanged()
+    {
+        var patientId = new Guid(EmergencyWardRootTestFixture.PatientId3);
+        var wardIdentifier = "M1";
+        var reason = "Transfer to another ward";
+
+        await Assert.ThrowsAnyAsync<Exception>(async () =>
+            await fixture.EmergencyWard.TransferPatient(patientId, wardIdentifier, reason, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task TransferPatient_ShouldFail_IfPatientDoesNotExist()
+    {
+        var patientId = Guid.NewGuid();
+        var wardIdentifier = "M1";
+        var reason = "Transfer to another ward";
+
+        await Assert.ThrowsAnyAsync<Exception>(async () =>
+            await fixture.EmergencyWard.TransferPatient(patientId, wardIdentifier, reason, CancellationToken.None));
     }
 }
 
